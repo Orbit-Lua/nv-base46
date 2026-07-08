29fb906a950ba20288c436fe6ec3d7e934f988df
abner <qw0207060413@gmail.com>
2026-07-08 09:47:21 +0800

    feat(statusline): improve default highlights

--- lua/base46/integrations/statusline/default.lua
+++ lua/base46/integrations/statusline/default.lua
@@ -1,10 +1,10 @@
 local config = require "nvconfig"
 local get_theme_tb = require("base46").get_theme_tb
 local colors = get_theme_tb "base_30"
-local generate_color = require("base46.colors").change_hex_lightness
+local color_utils = require "base46.colors"
 
 local statusline_bg = config.base46.transparency and "NONE" or colors.statusline_bg
-local light_grey = generate_color(colors.light_grey, 8)
+local light_grey = color_utils.change_hex_lightness(colors.light_grey, 8)
 
 local M = {
   StatusLine = { bg = statusline_bg },
@@ -17,7 +17,8 @@ local M = {
   St_cwd_icon = { fg = colors.one_bg, bg = colors.red },
   St_cwd_text = { fg = colors.white, bg = colors.lightbg },
   St_cwd_sep = { fg = colors.red, bg = statusline_bg },
-  St_pos_sep = { fg = colors.green, bg = colors.lightbg },
+  St_pos_sep = { fg = colors.green, bg = statusline_bg },
+  St_pos_sep_mid = { fg = colors.green, bg = colors.lightbg },
   St_pos_icon = { fg = colors.black, bg = colors.green },
   St_pos_text = { fg = colors.green, bg = colors.lightbg },
 
@@ -26,11 +27,17 @@ local M = {
   St_lspWarning = { fg = colors.yellow, bg = statusline_bg },
   St_LspHints = { fg = colors.purple, bg = statusline_bg },
   St_LspInfo = { fg = colors.green, bg = statusline_bg },
+
+  St_copilot = { fg = colors.cyan, bg = statusline_bg },
+  St_symbols = { fg = colors.white, bg = statusline_bg },
+  St_symbols_sep = { fg = colors.white, bg = statusline_bg },
+  St_file_path = { fg = colors.light_grey, bg = statusline_bg },
+  St_file_open = { fg = colors.white, bg = statusline_bg },
 }
 
 local function genModes_hl(modename, col)
   M["St_" .. modename .. "Mode"] = { fg = colors.black, bg = colors[col], bold = true }
-  M["St_" .. modename .. "ModeSep"] = { fg = colors[col], bg = colors.grey }
+  M["St_" .. modename .. "ModeSep"] = { fg = colors[col], bg = statusline_bg }
 end
 
 genModes_hl("Normal", "nord_blue")
